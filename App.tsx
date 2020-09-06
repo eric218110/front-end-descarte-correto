d1f0fbbb766206c8d73236072b52220ece72ef4d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-06 04:58:42 -0300

    feat: create home screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Home } from './src/screens/Home'
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <React.Fragment>
       <StatusBar style="auto" />
-    </View>
+      <Home />
+    </React.Fragment>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
 
 export default App
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+export const Home: React.FC = () => (
+  <View>
+    <SafeAreaView>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  </View>
+)
