9a779dcae71b987121c9f0fc72c3527595e97add
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-08 15:37:14 -0300

    refactor: import Home Screen

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
+import { Home as HomeScreen } from '../screens/Home'
 
 const Stack = createStackNavigator()
 
 export const AppRoutes: React.FC = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Home"
+        component={HomeScreen}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 )
