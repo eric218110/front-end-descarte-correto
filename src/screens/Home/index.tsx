248f1b21b5e61eee53babf43e1ac1360a05ea4f6
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-08 12:13:31 -0300

    feat: basic structure for Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import {
+  Container,
+  Wrapper,
+  Logo,
+  ImageLogo,
+  TextLogo,
+  Main,
+  ImageMain,
+  TextMain,
+  Bottom,
+  ButtonBottom
+} from './styles'
 
 export const Home: React.FC = () => (
-  <View>
-    <SafeAreaView>
-      <Text>Home Screen</Text>
-    </SafeAreaView>
-  </View>
+  <Wrapper>
+    <Container>
+      <Logo>
+        <ImageLogo />
+        <TextLogo>Any Name</TextLogo>
+      </Logo>
+      <Main>
+        <ImageMain />
+        <TextMain>Welcome my App</TextMain>
+        <TextMain>Let´s help our planet</TextMain>
+      </Main>
+      <Bottom>
+        <ButtonBottom>Find Collect points</ButtonBottom>
+      </Bottom>
+    </Container>
+  </Wrapper>
 )
