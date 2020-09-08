b0e8c2df44c0ec1d31df9dacaa80f9830bc3eb8d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-08 12:14:16 -0300

    style: basic structure for Home screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native'
+import { Dimensions } from 'react-native'
+
+const { height, width } = Dimensions.get('screen')
+
+export const Container = styled.View`
+  height: ${height}px;
+  width: ${width}px;
+  background-color: #fff;
+`
+export const Wrapper = styled.View``
+
+export const Logo = styled.View``
+
+export const ImageLogo = styled.View``
+
+export const TextLogo = styled.Text``
+
+export const Main = styled.View``
+
+export const ImageMain = styled.View``
+
+export const TextMain = styled.Text``
+
+export const Bottom = styled.View``
+
+export const ButtonBottom = styled.Text``
