fad656e22533760eb1946d36fd223ca4d4be551d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-08 15:36:45 -0300

    refactor: config expo fonts roboto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
+/* eslint-disable camelcase */
 import { StatusBar } from 'react-native'
 import React from 'react'
 import { Routes } from './src/routes'
+import { AppLoading } from 'expo'
+import {
+  useFonts,
+  Roboto_500Medium,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    roboto_400: Roboto_500Medium,
+    roboto_500: Roboto_400Regular,
+    roboto_700: Roboto_700Bold
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
     <React.Fragment>
       <StatusBar
