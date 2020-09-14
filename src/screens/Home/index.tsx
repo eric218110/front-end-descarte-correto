4ad8bd2089b971f4b454b3b7dace763c22bda666
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-14 03:00:30 -0300

    refactor: using alert

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+
 import {
   Container,
   Wrapper,
@@ -17,17 +18,16 @@ import HumanIcon from '../../assets/human.svg'
 import LogoIcon from '../../assets/logo.svg'
 import { useNavigation } from '@react-navigation/native'
 import NetInfo from '@react-native-community/netinfo'
-import { Alert } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
+import { AlertAnimated } from '../../components/Alert'
 
 export const Home: React.FC = () => {
   const navigation = useNavigation()
   const [isConnected, setIsConnected] = useState<boolean>()
   useEffect(() => {
     NetInfo.addEventListener(({ isInternetReachable }) => {
-      console.log(isInternetReachable)
       setIsConnected(!!isInternetReachable)
     })
-    console.log(isConnected)
   }, [])
 
   const handleNavigate = () => {
@@ -37,13 +37,21 @@ export const Home: React.FC = () => {
         index: 1,
         routes: [{ name: 'Maps' }]
       })
-    } else {
-      Alert.alert('No connection')
     }
   }
   return (
     <Wrapper>
       <Container>
+        {!isConnected && (
+          <AlertAnimated
+            title="No connection"
+            description="Verify you connection"
+            backgroundColor="#f2cccc"
+            colorActions="#FF0000"
+            iconName="wifi-off"
+          />
+        )}
+
         <Logo>
           <LogoIcon />
           <TextLogo>Any Name</TextLogo>
@@ -65,6 +73,9 @@ export const Home: React.FC = () => {
               <IconLeftButtom />
             </ContentRight>
           </ButtonBottom>
+          <TouchableOpacity onPress={() => setIsConnected(!isConnected)}>
+            <Text>Off</Text>
+          </TouchableOpacity>
         </Bottom>
       </Container>
     </Wrapper>
