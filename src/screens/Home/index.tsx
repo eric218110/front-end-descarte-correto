0b725c92d6485829ad6557d1a5aa724eeadd0dbe
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-14 00:23:39 -0300

    style: verify connection

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Container,
   Wrapper,
@@ -16,9 +16,31 @@ import {
 import HumanIcon from '../../assets/human.svg'
 import LogoIcon from '../../assets/logo.svg'
 import { useNavigation } from '@react-navigation/native'
+import NetInfo from '@react-native-community/netinfo'
+import { Alert } from 'react-native'
 
 export const Home: React.FC = () => {
   const navigation = useNavigation()
+  const [isConnected, setIsConnected] = useState<boolean>()
+  useEffect(() => {
+    NetInfo.addEventListener(({ isInternetReachable }) => {
+      console.log(isInternetReachable)
+      setIsConnected(!!isInternetReachable)
+    })
+    console.log(isConnected)
+  }, [])
+
+  const handleNavigate = () => {
+    if (isConnected) {
+      navigation.navigate('Maps')
+      navigation.reset({
+        index: 1,
+        routes: [{ name: 'Maps' }]
+      })
+    } else {
+      Alert.alert('No connection')
+    }
+  }
   return (
     <Wrapper>
       <Container>
@@ -34,9 +56,12 @@ export const Home: React.FC = () => {
           </TitleContent>
         </Main>
         <Bottom>
-          <ButtonBottom onPress={() => navigation.navigate('Maps')}>
+          <ButtonBottom
+            disable={!!isConnected}
+            onPress={() => handleNavigate()}
+          >
             <TextButtonBottom>Find Collect points</TextButtonBottom>
-            <ContentRight>
+            <ContentRight disable={!!isConnected}>
               <IconLeftButtom />
             </ContentRight>
           </ButtonBottom>
