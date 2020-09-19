44133caf93d5954a7c6cae62e8da74d3be25c45e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-19 19:06:08 -0300

    refactor: import add point

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Home as HomeScreen } from '../screens/Home'
 import { Map as MapScreen } from '../screens/Map'
+import { AddPoint as AddPointScreen } from '../screens/Point/Add'
 
 const Stack = createStackNavigator()
 
@@ -18,6 +19,11 @@ export const AppRoutes: React.FC = () => (
         options={{ headerShown: false }}
         name="Maps"
         component={MapScreen}
+      />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="AddPoint"
+        component={AddPointScreen}
       />
     </Stack.Navigator>
   </NavigationContainer>
