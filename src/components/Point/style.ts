cad5930aae2ddf3ea192d702ee6791f60afb6711
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-25 23:47:54 -0300

    style: create point component

--- src/components/Point/style.ts
+++ src/components/Point/style.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components/native'
+import Logo from '../../assets/logo-white.svg'
+import { Fontisto } from '@expo/vector-icons'
 
-export const Container = styled.View``
+export const Wrapper = styled.View`
+  height: 35px;
+  padding: 3px;
+`
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const PointContainer = styled(Fontisto).attrs(
+  (props: { backgroundColor: string }) => ({
+    name: 'map-marker',
+    size: 32,
+    color: props.backgroundColor
+  })
+)`
+  position: relative;
+  z-index: 2;
+`
+
+export const ImageContainer = styled(Logo)`
+  height: 20px;
+  width: 20px;
+  position: absolute;
+  z-index: 3;
+  left: 4px;
+  top: 5px;
+`
