8adff0f8083154fcb76d5bd29fdda151519bc68f
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-26 11:42:22 -0300

    feat: add screen Account

---
+++ src/screens/Account/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export const Account = (): JSX.Element => {
+  return (
+    <Container>
+      <Text>Account</Text>
+    </Container>
+  )
+}
