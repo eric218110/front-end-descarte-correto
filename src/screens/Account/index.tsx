0c46c72ba3b5a62de5f7236d22ebe75a5ddd4f12
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-26 18:09:59 -0300

    feat: add screen Account

--- src/screens/Account/index.tsx
+++ src/screens/Account/index.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-import { Container } from './styles'
+import React, { useCallback, useState } from 'react'
+import {
+  Container,
+  Header,
+  ContentTitle,
+  Title,
+  Subtitle,
+  SubtitleBottom,
+  Body,
+  Bottom,
+  CreateAccount,
+  ButtonTouchableOpacity,
+  TextTouchableOpacity
+} from './styles'
+import Woman from '../../assets/woman.svg'
+import { Button } from '../../components/Button'
 
 export const Account = (): JSX.Element => {
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleClickButtonLogin = useCallback(() => {
+    setLoading(true)
+  }, [])
+
   return (
     <Container>
-      <Text>Account</Text>
+      <Header>
+        <ContentTitle>
+          <Title>Descarte </Title>
+          <Title grenColor>correto</Title>
+        </ContentTitle>
+        <Subtitle>Vamos cuidar do nosso planeta</Subtitle>
+      </Header>
+      <Body>
+        <Woman />
+      </Body>
+      <Bottom>
+        <Button
+          onPress={handleClickButtonLogin}
+          loading={loading}
+          text="entrar"
+        />
+        <CreateAccount>
+          <SubtitleBottom>novo por aqui?</SubtitleBottom>
+          <ButtonTouchableOpacity>
+            <TextTouchableOpacity>criar conta</TextTouchableOpacity>
+          </ButtonTouchableOpacity>
+        </CreateAccount>
+      </Bottom>
     </Container>
   )
 }
--- src/screens/Account/styles.ts
+++ src/screens/Account/styles.ts
@@ -1,5 +1,62 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
+import { colors } from '../../styles/colors'
 
-export const Container = styled.View`
+export const Wrapper = styled.SafeAreaView``
 
-`;
+export const Container = styled.SafeAreaView`
+  background: ${colors.background};
+  flex: 1;
+  padding-top: 102px;
+`
+
+export const ContentTitle = styled.Text`
+  justify-content: center;
+`
+
+export const Title = styled.Text<{ grenColor?: boolean }>`
+  font-family: roboto_700;
+  font-size: 26px;
+  color: ${props => (props.grenColor ? colors.primary : '#000')};
+`
+
+export const Subtitle = styled.Text`
+  font-family: roboto_200;
+  color: ${colors.actions.success.dark};
+  font-size: 18px;
+  margin-top: 20px;
+`
+
+export const SubtitleBottom = styled.Text`
+  font-family: roboto_200;
+  color: ${colors.primary};
+  font-size: 13px;
+  margin-top: 20px;
+  text-transform: uppercase;
+`
+
+export const Header = styled.View`
+  align-items: center;
+`
+
+export const Body = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Bottom = styled.View`
+  align-items: center;
+  margin-bottom: 75px;
+`
+
+export const CreateAccount = styled.View``
+
+export const ButtonTouchableOpacity = styled.TouchableOpacity``
+
+export const TextTouchableOpacity = styled.Text`
+  font-family: roboto_700;
+  color: ${colors.primaryDark};
+  font-size: 15px;
+  margin-top: 5px;
+  text-transform: uppercase;
+`
