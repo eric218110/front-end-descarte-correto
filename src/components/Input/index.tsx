371e9d3676eb507cfde9998cca621c9882432ffd
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-27 20:18:25 -0300

    style: refactor input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,96 @@
-import React from 'react'
-import { Container, InputStyled, TextInput } from './style'
-import { TextInputProps } from 'react-native'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/display-name */
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState
+} from 'react'
+import { Container, InputStyled } from './style'
+import { TextInput, TextInputProps } from 'react-native'
+import { colors } from '../../styles/colors'
+import { useField } from '@unform/core'
 
-interface InputProps extends TextInputProps {
-  text: string
+interface PropsInputStyled extends TextInputProps {
+  name: string
+  Icon?: any
 }
 
-export const Input = ({ text, style, ...props }: InputProps): JSX.Element => {
-  return (
-    <Container style={style}>
-      <TextInput>{text}</TextInput>
-      <InputStyled {...props} />
-    </Container>
-  )
+interface InputValueReference {
+  value: string
 }
+
+interface RefElementInput {
+  focus: () => void
+}
+
+export const Input = forwardRef(
+  (
+    { name, Icon, ...rest }: PropsInputStyled,
+    ref: React.Ref<RefElementInput>
+  ) => {
+    const { registerField, defaultValue = '', fieldName, error } = useField(
+      name
+    )
+    const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
+    const inputElementRef = useRef<TextInput>(null)
+
+    useImperativeHandle(ref, () => ({
+      focus() {
+        inputElementRef.current?.focus()
+      }
+    }))
+
+    useEffect(() => {
+      registerField<string>({
+        name: fieldName,
+        ref: inputValueRef.current,
+        path: 'value',
+        setValue(ref: any, value) {
+          inputValueRef.current.value = value
+          inputElementRef.current?.setNativeProps({ text: value })
+        },
+        clearValue() {
+          inputValueRef.current.value = ''
+          inputElementRef.current?.clear()
+        }
+      })
+    }, [fieldName, registerField])
+
+    const [isFocused, setIsFocused] = useState<boolean>(false)
+    const [isFilled, setIsFilled] = useState<boolean>(false)
+
+    const handleIsFocused = useCallback(() => {
+      setIsFocused(true)
+    }, [])
+
+    const handleIsBlur = useCallback(() => {
+      setIsFocused(false)
+      setIsFilled(!!inputValueRef.current.value)
+    }, [])
+
+    const getColorField = useCallback((): string => {
+      if (isFocused) return colors.primary
+      if (isFilled) return colors.primary
+      if (error) return colors.actions.error.dark
+      return '#a1a1a1'
+    }, [error, isFocused, isFilled])
+
+    return (
+      <Container isFocused={isFocused} isError={!!error}>
+        {Icon && <Icon color={getColorField()} />}
+        <InputStyled
+          ref={inputElementRef}
+          placeholderTextColor={getColorField()}
+          defaultValue={defaultValue}
+          onChangeText={value => (inputValueRef.current.value = value)}
+          onFocus={handleIsFocused}
+          onBlur={handleIsBlur}
+          {...rest}
+        />
+      </Container>
+    )
+  }
+)
--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,27 +1,36 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 import { colors } from '../../styles/colors'
 
-export const Container = styled.View`
+export const Container = styled.View<{ isFocused: boolean; isError: boolean }>`
   width: 320px;
-  height: 42px;
+  height: 50px;
   background-color: ${colors.primaryLight};
   border-radius: 5px;
-  border: 1px solid ${colors.primary};
-`
-
-export const TextInput = styled.Text`
-  font-size: 10px;
-  color: ${colors.primary};
-  position: absolute;
-  text-transform: uppercase;
-  top: 0;
-  left: 10px;
-  letter-spacing: 1px;
+  border: 2px solid transparent;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  margin-bottom: 15px;
+  margin-top: 15px;
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: ${colors.primary};
+    `}
+  ${props =>
+    props.isError &&
+    css`
+      border-color: ${colors.actions.error.dark};
+    `}
 `
 
 export const InputStyled = styled.TextInput`
-  height: 90%;
+  height: 100%;
   padding-left: 10px;
-  padding-top: 10px;
   padding-right: 3px;
+  font-size: 18px;
+  width: 90%;
+  flex: 1;
+  color: ${colors.primary};
+  font-weight: bold;
 `
