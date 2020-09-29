dc829dc88c59ac9a37c9a7c8537675afbfb2c594
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-29 16:15:49 -0300

    refactor: finish screen add point

--- src/screens/Point/Add/AddPoint/index.tsx
+++ src/screens/Point/Add/AddPoint/index.tsx
@@ -1,10 +1,212 @@
-import React from 'react'
-import { Container, Text } from './styles'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { Input } from '../../../../components/Input'
+import { Form } from '@unform/mobile'
+import {
+  Container,
+  ContainerMap,
+  MapViewStyled,
+  Content,
+  Body,
+  InputGroup,
+  Bottom,
+  ContentLeft,
+  Title,
+  HelpText,
+  NextIcon,
+  IconLocation,
+  DirectionLocation,
+  HelpContainer
+} from './styles'
+import * as Location from 'expo-location'
+import { Loading } from '../../../../components/Loading'
+import { Alert } from 'react-native'
+import { CircleButton } from '../../../../components/Button'
+import * as Yup from 'yup'
+import {
+  geoLocationApi,
+  AddressComponentsData
+} from '../../../../service/api/geoLocation'
+import { FormHandles } from '@unform/core'
+import getValidationErrorsYup from '../../../../utils/getValidationErrorYup'
+import { useNavigation } from '@react-navigation/native'
+
+type AddressFormData = {
+  referencePoint: string
+  street: string
+  neighborhood: string
+  state: string
+  city: string
+  zipCode: string
+  latitude: string
+  longitude: string
+}
 
 export const AddPoint = (): JSX.Element => {
+  const [initialLocation, setInitialLocation] = useState<{
+    latitude: number
+    longitude: number
+  }>({
+    latitude: 0,
+    longitude: 0
+  })
+  const [address, setAddress] = useState<AddressComponentsData>(
+    {} as AddressComponentsData
+  )
+  const [loading, setLoading] = useState<boolean>(false)
+  const formRef = useRef<FormHandles>(null)
+  const navigator = useNavigation()
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync()
+
+      if (status !== 'granted') {
+        Alert.alert('Error', 'Permission location')
+        return
+      }
+
+      const location = await Location.getCurrentPositionAsync({})
+      const { latitude, longitude } = location.coords
+      setInitialLocation({ latitude, longitude })
+    }
+
+    loadPosition()
+  }, [])
+  useEffect(() => {
+    async function getAddressComponents() {
+      if (initialLocation.latitude !== 0) {
+        const currentLocation = await geoLocationApi({
+          latitude: String(initialLocation.latitude),
+          longitude: String(initialLocation.longitude)
+        })
+        setAddress(currentLocation)
+      }
+    }
+
+    getAddressComponents()
+  }, [initialLocation])
+
+  const onSubmitNextCreatePointScreen = useCallback(
+    async (data: AddressFormData) => {
+      try {
+        setLoading(true)
+        formRef.current?.setErrors({})
+        const schemasYup = Yup.object().shape({
+          referencePoint: Yup.string().required(
+            'Campo ponto de referência é obrigatório'
+          )
+        })
+
+        await schemasYup.validate(data, {
+          abortEarly: false
+        })
+        setLoading(false)
+        const addressParams: AddressFormData = {
+          referencePoint: data.referencePoint,
+          ...address
+        }
+        navigator.navigate('AddPointDetails', addressParams)
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrorsYup(error)
+          formRef.current?.setErrors(errors)
+          setLoading(false)
+          return ''
+        }
+      }
+    },
+    [address]
+  )
   return (
     <Container>
-      <Text>Add Point</Text>
+      {initialLocation.latitude !== 0 ? (
+        <>
+          <ContainerMap>
+            <MapViewStyled
+              showsUserLocation
+              showsMyLocationButton={false}
+              showsTraffic={false}
+              showsCompass={false}
+              showsBuildings={false}
+              showsIndoors={false}
+              showsScale={false}
+              showsIndoorLevelPicker={false}
+              showsPointsOfInterest={false}
+              zoomEnabled={false}
+              scrollEnabled={false}
+              initialRegion={{
+                latitude: initialLocation.latitude,
+                longitude: initialLocation.longitude,
+                latitudeDelta: 0.014,
+                longitudeDelta: 0.014
+              }}
+            />
+          </ContainerMap>
+          <Content>
+            <Form ref={formRef} onSubmit={onSubmitNextCreatePointScreen}>
+              <Body>
+                <InputGroup>
+                  {address.street ? (
+                    <>
+                      <Input
+                        placeholder="ponto de referência"
+                        Icon={DirectionLocation}
+                        name={'referencePoint'}
+                        autoCapitalize="words"
+                        returnKeyType="send"
+                        onSubmitEditing={() => formRef.current?.submitForm()}
+                      />
+                      <Input
+                        value={address.street}
+                        editable={false}
+                        Icon={IconLocation}
+                        name={'street'}
+                      />
+                      <Input
+                        value={address.neighborhood}
+                        editable={false}
+                        Icon={IconLocation}
+                        name={'neighborhood'}
+                      />
+                      <Input
+                        value={address.city}
+                        editable={false}
+                        Icon={IconLocation}
+                        name={'city'}
+                      />
+                      <Input
+                        value={address.state}
+                        editable={false}
+                        Icon={IconLocation}
+                        name={'state'}
+                      />
+                    </>
+                  ) : (
+                    <Loading />
+                  )}
+                </InputGroup>
+              </Body>
+              <Bottom>
+                <ContentLeft>
+                  <Title>DADOS DA LOCALIZAÇÃO DO PONTO DE DESCARTE</Title>
+                  <HelpContainer>
+                    <HelpText>AJUDA?</HelpText>
+                  </HelpContainer>
+                </ContentLeft>
+                <CircleButton
+                  onPress={() => {
+                    formRef.current?.submitForm()
+                  }}
+                  loading={loading}
+                  Icon={NextIcon}
+                />
+              </Bottom>
+            </Form>
+          </Content>
+        </>
+      ) : (
+        <Loading />
+      )}
     </Container>
   )
 }
--- src/screens/Point/Add/AddPoint/styles.ts
+++ src/screens/Point/Add/AddPoint/styles.ts
@@ -1,18 +1,73 @@
 import styled from 'styled-components/native'
 import { colors } from '../../../../styles/colors'
 import { Dimensions } from 'react-native'
+import { MaterialIcons, Entypo, Fontisto } from '@expo/vector-icons'
+
+import MapView from 'react-native-maps'
 
 const { height, width } = Dimensions.get('screen')
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   height: ${height}px;
   width: ${width}px;
-  justify-content: center;
+  background: ${colors.background};
+`
+export const ContainerMap = styled.View`
+  width: 100%;
+  height: 30%;
+`
+
+export const MapViewStyled = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`
+
+export const Content = styled.View`
+  width: 100%;
+  height: 60%;
   align-items: center;
-  background: ${colors.primary};
 `
-export const Text = styled.Text`
-  color: ${colors.text.light};
+
+export const Body = styled.ScrollView``
+
+export const InputGroup = styled.View``
+
+export const Bottom = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 15px;
+  height: 10%;
+`
+
+export const ContentLeft = styled.View``
+
+export const Title = styled.Text`
+  font-family: roboto_200;
+  font-size: 10px;
+  color: ${colors.text.dark};
+`
+
+export const HelpContainer = styled.TouchableOpacity``
+
+export const HelpText = styled.Text`
   font-family: roboto_700;
-  font-size: 40px;
+  font-size: 10px;
+  color: ${colors.primary};
+  letter-spacing: 2px;
 `
+
+export const IconLocation = styled(MaterialIcons).attrs(() => ({
+  name: 'location-city',
+  size: 28
+}))``
+
+export const DirectionLocation = styled(Entypo).attrs(() => ({
+  name: 'direction',
+  size: 28
+}))``
+
+export const NextIcon = styled(Fontisto).attrs(() => ({
+  name: 'arrow-right',
+  size: 18,
+  color: colors.text.light
+}))``
