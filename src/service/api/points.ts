ca0982d654e3f1897e9078d2b7bc172fc69903b6
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-30 20:44:44 -0300

    refactor: finish AddPoints Api

--- src/service/api/points.ts
+++ src/service/api/points.ts
@@ -1,40 +1,101 @@
 import api from './index'
+import { Point } from '../domain/point'
 
-type Items = {
-  id: string
-  title: string
-  color: string
-  active: boolean
-  activeColor: string
-}
-
-type Account = {
+type PickAccountIdNameEmail = {
   id: string
   name: string
   email: string
 }
 
-export type PointsProps = {
-  id: string
-  name: string
-  latitude: string
-  longitude: string
-  city: string
-  state: string
-  image: string
-  phone: string
-  account: Account
-  items: Items[]
+export interface ResponseListPoints extends Omit<Point, 'account'> {
+  account: PickAccountIdNameEmail
 }
 
-export const getPointsApi = async (): Promise<PointsProps[]> => {
+export const getPointsApi = async (): Promise<ResponseListPoints[]> => {
   try {
-    const { data } = await api.get<PointsProps[]>('points')
+    const { data } = await api.get<ResponseListPoints[]>('points')
     if (!data) {
       return []
     }
     return data
   } catch (error) {
     return []
+  }
+}
+
+export type ResponseAddPointsProps = {
+  error: string
+  data: boolean
+}
+export interface AddPointsProps
+  extends Omit<Point, 'items' | 'account' | 'id' | 'image'> {
+  items: string
+  file: string
+  token: string
+}
+
+export const addPointApi = async ({
+  token,
+  file,
+  city,
+  items,
+  latitude,
+  longitude,
+  name,
+  neighborhood,
+  reference,
+  state,
+  street,
+  zipCode
+}: AddPointsProps): Promise<ResponseAddPointsProps> => {
+  try {
+    const fileData = {
+      uri: file,
+      type: 'image/jpeg',
+      name: Math.random().toString() + '-file-image-hash.jpg'
+    }
+    const bodyRequest = new FormData()
+    bodyRequest.append('file', fileData)
+    bodyRequest.append('city', city)
+    bodyRequest.append('items', items)
+    bodyRequest.append('latitude', latitude)
+    bodyRequest.append('longitude', longitude)
+    bodyRequest.append('name', name)
+    bodyRequest.append('neighborhood', neighborhood)
+    bodyRequest.append('reference', reference)
+    bodyRequest.append('state', state)
+    bodyRequest.append('street', street)
+    bodyRequest.append('zipCode', zipCode)
+
+    await api.post('point', bodyRequest, {
+      headers: {
+        'x-access-token': token
+      }
+    })
+    return {
+      data: true,
+      error: ''
+    }
+  } catch (error) {
+    if (error.response) {
+      return {
+        data: false,
+        error: error.response.data.error
+      }
+    } else if (error.request) {
+      return {
+        data: false,
+        error: 'Erro ao cadastrar, tente novamente'
+      }
+    } else {
+      return {
+        data: false,
+        error: 'Erro ao cadastrar, tente novamente em instantes'
+      }
+    }
+  }
+  return {
+    data: false,
+    error: 'Erro ao cadastrar'
   }
 }
