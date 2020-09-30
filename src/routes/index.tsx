5c7d1e6a804776462ee4d88ac5c6f0b579d387a1
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-30 20:47:09 -0300

    feat: add PointProvider in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { AppRoutes } from './app-routes'
 import { AccountProvider } from '../service/context/account-context'
+import { PointProvider } from '../service/context/point-context'
 
 export const Routes: React.FC = () => (
   <AccountProvider>
-    <AppRoutes />
+    <PointProvider>
+      <AppRoutes />
+    </PointProvider>
   </AccountProvider>
 )
