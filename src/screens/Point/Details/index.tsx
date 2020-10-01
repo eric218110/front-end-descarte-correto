063a8b9fb51ac3d30633e84f025a06f1c831d684
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-09-30 22:23:21 -0300

    feat: add DetailsPoint screen

---
+++ src/screens/Point/Details/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export const DetailsPoint: React.FC = () => {
+  return (
+    <Container>
+      <Text>Details</Text>
+    </Container>
+  )
+}
