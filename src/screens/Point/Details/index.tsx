d73fb9d37b9708c82747c2b7e496d8d5d19d1443
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-01 01:57:05 -0300

    feat: finish DetailsPoint screen

--- src/screens/Point/Details/index.tsx
+++ src/screens/Point/Details/index.tsx
@@ -1,12 +1,184 @@
-import React from 'react'
-import { Text } from 'react-native'
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { ResponseListPoints, getOnePointApi } from '../../../service/api/points'
+import { Loading } from '../../../components/Loading'
+import { AlertAnimated } from '../../../components/Alert'
+import { colors } from '../../../styles/colors'
+import {
+  Container,
+  ImageContainer,
+  Body,
+  ContentBody,
+  Header,
+  TextCreateBy,
+  Stars,
+  TextTitle,
+  TextDescription,
+  Divider,
+  ContentBox,
+  RowAddressInfo,
+  TextStrong,
+  ItemContent,
+  NameItem,
+  ContainerActionRouteButton,
+  IconActionRouteButton,
+  StarIcon,
+  StarIconOutline
+} from './styles'
+import { StatusBar } from 'react-native'
 
-import { Container } from './styles'
+type PointDataScreenParams = RouteProp<
+  Record<'PointDataScreenParams', { id: string }>,
+  'PointDataScreenParams'
+>
+
+type ErrorAlert = {
+  active: boolean
+  title: string
+  description: string
+}
 
 export const DetailsPoint: React.FC = () => {
+  const { id } = useRoute<PointDataScreenParams>().params
+  const { goBack } = useNavigation()
+  const [point, setPoint] = useState<ResponseListPoints | null>(null)
+  const [alert, setActiveAlert] = useState<ErrorAlert>({} as ErrorAlert)
+
+  useEffect(() => {
+    if (!id) goBack()
+    setActiveAlert({
+      active: false,
+      description: '',
+      title: ''
+    })
+  }, [])
+
+  useEffect(() => {
+    async function getPointWitId() {
+      const { data } = await getOnePointApi({ id })
+      if (data) {
+        setPoint(data)
+      } else {
+        handleAlertError({
+          description: 'Não foi possivel listar o ponto',
+          title: 'Erro ao listar'
+        })
+        setTimeout(() => {
+          goBack()
+        }, 5500)
+      }
+    }
+    getPointWitId()
+  }, [])
+
+  const handleAlertError = useCallback(
+    ({ description, title }: { description: string; title: string }) => {
+      setActiveAlert({
+        active: true,
+        description,
+        title
+      })
+      setTimeout(() => {
+        setActiveAlert({
+          active: false,
+          description: '',
+          title: ''
+        })
+      }, 5000)
+      return ''
+    },
+    [alert]
+  )
+
   return (
     <Container>
-      <Text>Details</Text>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <>
+        {point ? (
+          <Container>
+            <ImageContainer
+              source={{
+                uri: point.image
+              }}
+            />
+            <Header>
+              <TextCreateBy>
+                <TextDescription>ponto de coleta criado por </TextDescription>
+                <TextStrong>{point.account.name}</TextStrong>
+              </TextCreateBy>
+            </Header>
+            <Stars>
+              <StarIcon />
+              <StarIcon />
+              <StarIcon />
+              <StarIconOutline />
+              <StarIconOutline />
+            </Stars>
+            <ContentBody>
+              <Body>
+                <ContentBox>
+                  <TextTitle>descrição</TextTitle>
+                  <TextDescription>{point.name}</TextDescription>
+                </ContentBox>
+                <Divider />
+                <ContentBox>
+                  <TextTitle>endereço</TextTitle>
+                  <RowAddressInfo>
+                    <TextStrong>Rua: </TextStrong>
+                    <TextDescription>{point.street}</TextDescription>
+                  </RowAddressInfo>
+                  <RowAddressInfo>
+                    <TextStrong small>Bairro: </TextStrong>
+                    <TextDescription small>
+                      {point.neighborhood}
+                    </TextDescription>
+                  </RowAddressInfo>
+                  <RowAddressInfo>
+                    <TextStrong small>cidade: </TextStrong>
+                    <TextDescription small>{point.city}</TextDescription>
+                  </RowAddressInfo>
+                  <RowAddressInfo>
+                    <TextStrong small>cep: </TextStrong>
+                    <TextDescription small>{point.zipCode}</TextDescription>
+                  </RowAddressInfo>
+                </ContentBox>
+                <Divider />
+                <ContentBox>
+                  <TextTitle>ponto de REFERÊNCIA</TextTitle>
+                  <TextDescription>{point.reference}</TextDescription>
+                </ContentBox>
+                <Divider />
+                <ContentBox endList>
+                  <TextTitle>recebe os items</TextTitle>
+                  {point.items.map(item => (
+                    <ItemContent background={item.color} key={item.id}>
+                      <NameItem color={item.activeColor}>{item.title}</NameItem>
+                    </ItemContent>
+                  ))}
+                </ContentBox>
+              </Body>
+            </ContentBody>
+            <ContainerActionRouteButton>
+              <IconActionRouteButton />
+            </ContainerActionRouteButton>
+          </Container>
+        ) : (
+          <Loading />
+        )}
+        {alert.active && (
+          <AlertAnimated
+            title={alert.title}
+            description={alert.description}
+            backgroundColor={colors.actions.error.light}
+            colorActions={colors.actions.error.dark}
+            iconName="alert"
+          />
+        )}
+      </>
     </Container>
   )
 }
--- src/screens/Point/Details/styles.ts
+++ src/screens/Point/Details/styles.ts
@@ -1,5 +1,147 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
+import { colors } from '../../../styles/colors'
+import { Dimensions } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-export const Container = styled.View`
+const { height, width } = Dimensions.get('window')
 
-`;
+export const Container = styled.SafeAreaView`
+  height: ${height}px;
+  width: ${width}px;
+  background: ${colors.background};
+  justify-content: center;
+  align-items: center;
+`
+
+export const ImageContainer = styled.Image.attrs(() => ({
+  imageStyle: {
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    resizeMode: 'center'
+  }
+}))`
+  width: 100%;
+  height: 30%;
+`
+
+export const Body = styled.ScrollView`
+  height: 70%;
+  width: 100%;
+  padding-bottom: 20px;
+`
+
+export const ContentBody = styled.View`
+  flex: 1;
+`
+
+export const Header = styled.View``
+
+export const TextCreateBy = styled.TouchableOpacity`
+  flex-direction: row;
+`
+
+export const Stars = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+  width: 320px;
+  padding-bottom: 20px;
+`
+
+export const StarIcon = styled(MaterialCommunityIcons).attrs(() => ({
+  name: 'star',
+  size: 32,
+  color: '#FFC805'
+}))``
+
+export const StarIconOutline = styled(MaterialCommunityIcons).attrs(() => ({
+  name: 'star-outline',
+  size: 32,
+  color: '#FFC805'
+}))``
+
+export const ContentBox = styled.View<{ endList?: boolean }>`
+  width: 320px;
+  padding: 10px;
+  background: ${colors.secundary};
+  justify-content: center;
+  border-radius: 5px;
+  margin-bottom: ${props => (!props.endList ? '2px' : '60px')};
+`
+
+export const TextTitle = styled.Text`
+  font-family: roboto_500;
+  font-size: 10px;
+  color: ${colors.text.light};
+  text-transform: uppercase;
+  position: absolute;
+  left: 10px;
+  top: 2px;
+`
+
+export const TextDescription = styled.Text<{ small?: boolean }>`
+  font-family: roboto_500;
+  font-size: 15px;
+  color: ${colors.primary};
+  margin-top: ${props => (props.small ? 0 : '7px')};
+`
+
+export const Divider = styled.View`
+  width: 300px;
+  background: #d1d1d1;
+  height: 1px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-left: 10px;
+`
+
+export const RowAddressInfo = styled.View`
+  flex-direction: row;
+`
+
+export const TextStrong = styled.Text<{ small?: boolean }>`
+  font-family: roboto_700;
+  font-size: 15px;
+  color: ${colors.primary};
+  text-transform: uppercase;
+  margin-top: ${props => (props.small ? 0 : '7px')};
+`
+
+export const ItemContent = styled(RectButton)<{
+  background: string
+}>`
+  width: 100%;
+  height: 39px;
+  border-radius: 5px;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 10px;
+  background: ${props => props.background};
+`
+
+export const NameItem = styled.Text<{ color: string }>`
+  margin-left: 24px;
+  color: ${props => props.color};
+  font-size: 17px;
+  font-family: roboto_700;
+`
+
+export const ContainerActionRouteButton = styled(RectButton)`
+  position: absolute;
+  bottom: 38px;
+  right: 21px;
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+  background: ${colors.primary};
+  align-items: center;
+  justify-content: center;
+`
+
+export const IconActionRouteButton = styled(MaterialCommunityIcons).attrs(
+  () => ({
+    name: 'directions',
+    size: 32,
+    color: colors.text.light
+  })
+)``
