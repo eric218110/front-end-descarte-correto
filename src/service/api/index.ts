47176bb1c6b324e8785ccf89878baa495476863a
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-19 11:30:16 -0300

    refactor: add ResponseType<T>

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+export interface ResponseType<T> {
+  error: string
+  dataResponse: T | null
+}
+
 const api = axios.create({
   baseURL: 'http://10.0.0.121:1995/api'
   // baseURL: 'http://35.198.29.33/api'
