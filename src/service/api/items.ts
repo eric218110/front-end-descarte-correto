3e415b066a38ff3483abd026e11e8972851fd036
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-10-19 11:30:41 -0300

    feat: create function for add new item

--- src/service/api/items.ts
+++ src/service/api/items.ts
@@ -1,21 +1,70 @@
-import api from './index'
+import api, { ResponseType } from './index'
+import { Item } from '../domain/item'
 
-export type IStateItems = {
-  id: string
-  title: string
-  color: string
-  active: boolean
-  activeColor: string
-}
-
-export const getItemsApi = async (): Promise<IStateItems[]> => {
+export const getItemsApi = async (): Promise<Item[]> => {
   try {
-    const { data } = await api.get<IStateItems[]>('item')
+    const { data } = await api.get<Item[]>('item')
     if (!data) {
       return []
     }
     return data
   } catch (error) {
     return []
+  }
+}
+
+export interface AddItemProps extends Omit<Item, 'id' | 'active'> {
+  accessToken: string
+}
+
+export const addItemApi = async ({
+  title,
+  description,
+  color,
+  activeColor,
+  accessToken
+}: AddItemProps): Promise<ResponseType<Item>> => {
+  try {
+    const { data } = await api.post<Item>(
+      'item',
+      {
+        title,
+        description,
+        color,
+        activeColor
+      },
+      {
+        headers: {
+          'x-access-token': accessToken
+        }
+      }
+    )
+    if (!data) {
+      return {
+        error: '',
+        dataResponse: {} as Item
+      }
+    }
+    return {
+      error: '',
+      dataResponse: {} as Item
+    }
+  } catch (error) {
+    if (error.response) {
+      return {
+        dataResponse: null,
+        error: error.response.data.error
+      }
+    } else if (error.request) {
+      return {
+        dataResponse: null,
+        error: 'Erro ao cadastrar, tente novamente'
+      }
+    } else {
+      return {
+        dataResponse: null,
+        error: 'Erro ao cadastrar'
+      }
+    }
   }
 }
