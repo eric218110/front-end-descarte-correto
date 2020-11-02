7b45c78f44272ac5304a252d9d784abb6c0014b4
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-11-02 13:15:31 -0300

    refactor: change name import

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { registeredRoutes } from './routes'
+import { registeredRoutes } from './register-routes'
 
 const Stack = createStackNavigator()
 
